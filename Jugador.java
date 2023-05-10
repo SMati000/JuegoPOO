3aebff63e3d03ccf7775f3cfb300311b6fe1c8b4
mati.s <matiastschulz@gmail.com>
2023-05-10 09:53:40 -0300

    Aniadir clases de los jugadores
    	* Superclase abstracta Jugador
    	* Subclase Jugador1943
    	* Modificar diagrama UML acorde

---
+++ Jugador.java
@@ -0,0 +1,7 @@
+public abstract class Jugador {
+    protected final String nombre;
+    
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ Jugador1943.java
@@ -0,0 +1,7 @@
+public class Jugador1943 extends Jugador {
+    private int puntaje;
+
+    public Jugador1943(String nombre) {
+        super(nombre);
+    }
+}
