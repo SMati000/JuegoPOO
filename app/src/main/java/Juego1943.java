15dc3ba213f90ae001287d6795fa70649aff5608
mati.s <matiastschulz@gmail.com>
2023-05-10 19:36:47 -0300

    Se rearmo la estructura del proyecto para usar gradle
    * Se incorporaron las librerias de la DB y del Loop del juego

--- app/src/main/java/Juego1943.java
+++ app/src/main/java/Juego1943.java
@@ -3,7 +3,7 @@ import java.awt.*;
 public class Juego1943 extends Juego{
 
     public Juego1943(){
-        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        //setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 
 
 
