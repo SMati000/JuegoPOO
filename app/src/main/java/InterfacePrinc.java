cbf99d37cbb96bf84c428f6689eb4b1a7b17c100
mati.s <matiastschulz@gmail.com>
2023-05-14 21:38:24 -0300

    Se acomodaron detalles del codigo

--- app/src/main/java/InterfacePrinc.java
+++ app/src/main/java/InterfacePrinc.java
@@ -1,121 +1,77 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
-
-
-
 class InterfacePrinc extends JFrame implements ActionListener{
-    JPanel panel, panel2, panel3, panel4;
-    JButton b1, b2, b3, b4;
-    JMenuItem item1,item2, item3;
+    JPanel panel, panelFondo = new JPanel();
+    JButton b1, bGenerico;
     JMenuBar menu;
     JMenu menu1, menu2;
-    JPanel panelFondo = new JPanel();
-    
+    JMenuItem item1,item2, item3;
 
     public static void main(String[] args) {  
         new InterfacePrinc();
     } 
 
     public InterfacePrinc() {
+        this.setPreferredSize(new Dimension(450, 550));
+        this.setLayout(new GridBagLayout());
+        
         mostrarCatalogo();
+
+        this.setVisible(true);
+        this.pack();  
         
-    }
-/*
-    public void paint(Graphics g){
-        Dimension dimension = this.getSize();
-        ImageIcon icon = new ImageIcon(getClass().getResource("/src/main/resource/imagenes/Fondo.jpg"));
-        g.drawImage(icon.getImage(), 0, 0, 1080, 720, null);
-        
-        panelFondo.setOpaque(false);
-        super.paintComponents(g);
-    }
-*/
-    public void mostrarCatalogo(){
-        setLayout(null);
-       
-        
-
-        
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.setBounds(50,50,150,200);
-        panel.setBackground(Color.gray);
-        panel.add(new JLabel("1943"), BorderLayout.NORTH);
-        b1 = new JButton("Iniciar");
-        panel.add(b1, BorderLayout.SOUTH);
-      
-
-        panel2 = new JPanel();
-        panel2.setLayout(new BorderLayout());
-        panel2.setBounds(210,50,150,200);
-        panel2.setBackground(Color.gray);
-        panel2.add(new JLabel("Juego 2"), BorderLayout.NORTH);                  //ver bien layout de frame, repiten borderLayout.NORTH
-        b2 = new JButton("Iniciar");
-        panel2.add(b2, BorderLayout.SOUTH);
-
-
-        panel3 = new JPanel();
-        panel3.setLayout(new BorderLayout());
-        panel3.setBounds(50,270,150,200);
-        panel3.setBackground(Color.gray);
-        panel3.add(new JLabel("Juego 3"), BorderLayout.NORTH);
-        b3 = new JButton("Iniciar");
-        panel3.add(b3, BorderLayout.SOUTH);
-
-
-        panel4 = new JPanel();
-        panel4.setLayout(new BorderLayout());
-        panel4.setBounds(210,270,150,200);
-        panel4.setBackground(Color.gray);
-        panel4.add(new JLabel("Juego 4"), BorderLayout.NORTH);
-        b4 = new JButton("Iniciar");
-        panel4.add(b4, BorderLayout.SOUTH);
-
-        
-
-
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-        b3.addActionListener(this);
-        b4.addActionListener(this);
-        
-
-        this.add(panel);
-        this.add(panel2);
-        this.add(panel3);
-        this.add(panel4);
-        this.add(panelFondo);   
-        
-
-        setVisible(true);
-        pack();
-
-
-        
-        WindowAdapter l=new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                     System.exit(0);
             };
-    
-        };
-        this.addWindowListener(l);
-        //Agrega el objeto para que se muestre por la ventana.
+        });
+    }
 
-        Dimension d = new Dimension(450,550);
-        this.setPreferredSize(d);
-       
-        //Redimensiona la ventana a su tama�o natural
-        this.setVisible(true);
-        this.pack();
+    public void mostrarCatalogo() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = c.gridy = 0;
+        c.ipadx = 120;
+        c.ipady = 180;
+        c.insets = new Insets(10, 10, 10, 10);
 
+        panel = new JPanel(new BorderLayout());
+
+        panel.setBackground(Color.gray);        
+        panel.add(new JLabel("1943"));
+        
+        b1 = new JButton("Iniciar");
+        panel.add(b1, BorderLayout.SOUTH);
+        b1.addActionListener(this);
+        
+        this.add(panel, c);
+
+        for(int j = 0; j <= 1; j++) {
+            for(int i = j == 0 ? 2 : 1; i <= 2; i++) {
+                c.gridx = (i-1);
+                c.gridy = j;
+                
+                JPanel juegoGenerico = new JPanel(new BorderLayout());
+
+                juegoGenerico.setBackground(Color.gray);
+                juegoGenerico.add(new JLabel("Juego " + (i+j*2)));
+                
+                bGenerico = new JButton("Iniciar");
+                juegoGenerico.add(bGenerico, BorderLayout.SOUTH);
+                bGenerico.addActionListener(this);
+                
+                this.add(juegoGenerico, c);
+            }
+        }
+
+        this.add(panelFondo);
     }
 
     public void actionPerformed(ActionEvent evento){
         JPanel panelJuego = new JPanel();
         JFrame frame = new JFrame();
-        
         
         if (evento.getSource()==item1) {
             JFrame framConfTecla;
@@ -128,34 +84,24 @@ class InterfacePrinc extends JFrame implements ActionListener{
 
             Configuraciones conf = new Configuraciones();
 
-            
+            String opciones[] = {"a","b", "c", "d" ,"e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        
             framConfTecla = new JFrame("Configuracion de teclas");
+
             framConfTecla.setLayout(new GridLayout(7,7));
-            Dimension d1 = new Dimension(300,400);
-            framConfTecla.setPreferredSize(d1);
+            framConfTecla.setPreferredSize(new Dimension(300,400));
 
             lb1 = new JLabel("Hacia adelante: ");
+
             ch1 = new Choice();
-            String opciones[] = {"a","b", "c", "d" ,"e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-            ch1.addItem("↑");                                   //muestra un cuadrado, cambiarlo
+            ch1.add("↑"); //muestra un cuadrado, cambiarlo
             for (int i = 0; i < opciones.length; i++) {
                 ch1.add(opciones[i]);
             }
+
             framConfTecla.add(lb1);
             framConfTecla.add(ch1);
-
-            ch7.addKeyListener(new KeyAdapter() {
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-                   // System.out.println(KeyEvent.getKeyText(e.getExtendedKeyCode()) + " - " + e.getExtendedKeyCode());
-                    conf.guardarEnBD(e.getExtendedKeyCode(), KeyEvent.getKeyText(e.getExtendedKeyCode()));
-                    ch7.setText(KeyEvent.getKeyText(e.getExtendedKeyCode()));
-                    // habria q hacer que se guarde en la DB
-                }
-            });
             
-       
             ch1.addItemListener(new ItemListener(){
                 public void itemStateChanged(ItemEvent ie){
                    System.out.println(KeyEvent.getExtendedKeyCodeForChar(ch1.getSelectedItem().toCharArray()[0]) + "");
@@ -168,7 +114,7 @@ class InterfacePrinc extends JFrame implements ActionListener{
 
             lb2 = new JLabel("Hacia abajo: ");
             ch2 = new Choice();
-            ch2.addItem("↓");                                   //muestra un cuadrado, cambiarlo
+            ch2.add("↓");                                   //muestra un cuadrado, cambiarlo
             for (int i = 0; i < opciones.length; i++) {
                 ch2.add(opciones[i]);
             }
@@ -185,7 +131,7 @@ class InterfacePrinc extends JFrame implements ActionListener{
             
             lb3 = new JLabel("Derecha: ");
             ch3 = new Choice();
-            ch3.addItem("→");                                   //muestra un cuadrado, cambiarlo
+            ch3.add("→");                                   //muestra un cuadrado, cambiarlo
             for (int i = 0; i < opciones.length; i++) {
                 ch3.add(opciones[i]);
             }
@@ -201,7 +147,7 @@ class InterfacePrinc extends JFrame implements ActionListener{
 
             lb4 = new JLabel("Izquierda: ");
             ch4 = new Choice();
-            ch4.addItem("←");                                   //muestra un cuadrado, cambiarlo
+            ch4.add("←");                                   //muestra un cuadrado, cambiarlo
             for (int i = 0; i < opciones.length; i++) {
                 ch4.add(opciones[i]);
             }
@@ -216,7 +162,7 @@ class InterfacePrinc extends JFrame implements ActionListener{
            
             lb5 = new JLabel("Disparar: ");
             ch5 = new Choice();
-            ch5.addItem("x");                                   
+            ch5.add("x");                                   
             for (int i = 0; i < opciones.length; i++) {
                 ch5.add(opciones[i]);
             }
@@ -232,7 +178,7 @@ class InterfacePrinc extends JFrame implements ActionListener{
 
             lb6 = new JLabel("Ataques especiales: ");
             ch6 = new Choice();
-            ch6.addItem("z");                                   
+            ch6.add("z");                                   
             for (int i = 0; i < opciones.length; i++) {
                 ch6.add(opciones[i]);
             }
@@ -253,6 +199,17 @@ class InterfacePrinc extends JFrame implements ActionListener{
             // }
             framConfTecla.add(lb7);
             framConfTecla.add(ch7);
+
+            ch7.addKeyListener(new KeyAdapter() {
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                   // System.out.println(KeyEvent.getKeyText(e.getExtendedKeyCode()) + " - " + e.getExtendedKeyCode());
+                    conf.guardarEnBD(e.getExtendedKeyCode(), KeyEvent.getKeyText(e.getExtendedKeyCode()));
+                    ch7.setText(KeyEvent.getKeyText(e.getExtendedKeyCode()));
+                    // habria q hacer que se guarde en la DB
+                }
+            });
             
 
 /*
@@ -270,12 +227,9 @@ class InterfacePrinc extends JFrame implements ActionListener{
         if(evento.getSource()== item2){
             JFrame framConfSonido = new JFrame();
             framConfSonido.setLayout(new GridLayout(2,1));
-            Dimension d1 = new Dimension(300,400);
-            framConfSonido.setPreferredSize(d1);
+            framConfSonido.setPreferredSize(new Dimension(300,400));
            
-           
-            JLabel defecto= new JLabel("Sonido por defecto");                   //nombre de la pista
-
+            JLabel defecto= new JLabel("Sonido por defecto");      //nombre de la pista
             JLabel nuevoPista = new JLabel("Cambiar musica:");    //como se pone nueva musica en juego??
             JTextField url = new JTextField("Url nueva musica");
 
@@ -309,36 +263,36 @@ class InterfacePrinc extends JFrame implements ActionListener{
             int ipadx = 10;
             int ipady = 10;
         
-            JButton jugar =  new JButton("JUGAR");
             GridBagConstraints resBotonJugar = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 
                                                                 weightx, weighty, anchor, fill,in, ipadx, ipady);
 
-                        
+            JButton jugar =  new JButton("JUGAR");
             jugar.addActionListener(this);
             frameJuego.add(jugar, resBotonJugar);
 
-
             //menu visto en pantalla
             menu = new JMenuBar();
-            setJMenuBar(menu);
+
             menu1 = new JMenu("Configuraciones");
             menu2 = new JMenu("Ver");
+            
             menu.add(menu1); 
             menu.add(menu2); 
 
             //item dentro de configuraciones
             item1 = new JMenuItem("Teclado");
-            menu1.add(item1);
-            // item1.addActionListener(this);
             item1.addActionListener(this);
+            menu1.add(item1);
+
             item2 = new JMenuItem("Sonido");
-            menu1.add(item2);
             item2.addActionListener(this);
-            item3 = new JMenuItem("Avion");         
+            menu1.add(item2);
+
+            item3 = new JMenuItem("Avion");     
+            item3.addActionListener(this);    
             menu1.add(item3);
-            item3.addActionListener(this);
        
-            frameJuego.add(menu);
+            frameJuego.setJMenuBar(menu);
             
             jugar.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evt) {
@@ -351,23 +305,14 @@ class InterfacePrinc extends JFrame implements ActionListener{
                         };
             
                         t.start();
-                        
                     }
                 }
             });
 
-
-
-           
-
             frameJuego.setVisible(true);
             frameJuego.pack();
             
-
-
-
-            
-        } else if(evento.getActionCommand() == b2.getActionCommand()) {
+        } else if(evento.getActionCommand() == bGenerico.getActionCommand()) {
             panelJuego.add(new JLabel("Cargando..."));
             panelJuego.setBackground(Color.gray);
             Dimension dim = new Dimension(100,100);
@@ -376,10 +321,5 @@ class InterfacePrinc extends JFrame implements ActionListener{
             frame.setVisible(true);
             frame.pack();
         }
-        
-
     }
-
-
-
 }
